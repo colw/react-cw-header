a28655e22d4cd996fb7e3ff0b2db4733ec4df191
colw <colw@outlook.com>
2017-02-07 12:11:19 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# React CW Header Bar
+
+A React based header bar. It contains a navigation burger menu.
+
+WIP
---
+++ index.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import classNames from 'classnames';
+import { Link } from 'react-router';
+
+
+require('./style.css');
+
+function capitalise(t) {
+  return t[0].toUpperCase() + t.slice(1);
+}
+
+let MenuItem = ({text, clickItem}) => (
+  <li>
+    <Link activeClassName="active" onClick={() => this.setState({open: false})}>
+      {capitalise(text)}
+    </Link>
+  </li>
+)
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+    }
+    this.toggle = this.toggle.bind(this);
+    this.closeMenu = this.closeMenu.bind(this);
+  }
+  toggle() {
+    this.setState({open: !this.state.open}, 
+      () => {console.debug(this.state.open)});
+  }
+  closeMenu(e) {
+    this.setState({open: false}, 
+      () => {console.debug(this.state.open)});
+  }
+  render() {
+    let makeList = (x,y) => (
+      <li key={y}>
+        <Link to={`/items/${x}`} activeClassName="active" onClick={this.closeMenu} onlyActiveOnIndex={true}>
+          {capitalise(x)}
+        </Link>
+      </li>
+    )
+
+    return (
+      <div className="header-container">
+        <div className="header-bar">
+          <span className="menu-icon left" onClick={this.toggle}>
+            <i className={"fa " + (this.state.open ? "fa-close" : "fa-bars")} aria-hidden="true"></i>
+          </span>
+          <h1 className="header-title">{this.props.title}</h1>
+        </div>
+        <div className={"menu-container " + (this.state.open ? "open" : "")}>
+          <ul>
+            <li key={this.props.items.length}>
+              <Link to={`/`} activeClassName="active" onClick={this.closeMenu}  onlyActiveOnIndex={true}>
+                Home
+              </Link>
+            </li>
+            {
+              this.props.items.map(makeList)
+            }
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Header;
